033ab7fabec6d731bd3b6960520bdfbeb391967f
brain-eater <34330361+brain-eater@users.noreply.github.com>
2019-02-22 14:15:30 +0530

    added executable permission to hooks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,13 @@ set -e
 
 echo "#Setup-- Creating git's precomit hook"
 cp .github/git_pre_commit_hook .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
 echo "#Setup-- Creating git's pre-rebase hook"
 cp .github/git_pre_rebase_hook .git/hooks/pre-rebase
+chmod +x .git/hooks/pre-rebase
 echo "#Setup-- Creating git's pre-push hook"
 cp .github/git_pre_push_hook .git/hooks/pre-push
-chmod +x .git/hooks/pre-commit
+chmod +x .git/hooks/pre-push
 echo "#Setup-- Installing node dependencies"
 npm install
 echo "#Setup-- Setting commit template in git config"
