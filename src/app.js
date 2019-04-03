cf754f1b4d33ef1d3b856b113fe8ba042851802d
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-04-03 18:49:40 +0530

    Rearranged app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const app = express();
-const fs = require("fs");
 const { createGameId } = require("./utils/utils");
 const { renderHomePage, getCurrentGame, logRequest } = require("./handlers.js");
 const {
@@ -11,6 +10,8 @@ const {
   cancelGame,
   provideGameLobby
 } = require("./hostAndJoinHandlers");
+
+const app = express();
 
 const readSavedGames = () => {
   if (!fs.existsSync("./data/savedGames.json")) {
@@ -26,9 +27,9 @@ const loadSavedGames = function() {
 
 const {
   getGame,
-  addCashFlow,
   payDebt,
   rollDice,
+  passDeal,
   saveGame,
   hasShares,
   buyShares,
@@ -38,13 +39,17 @@ const {
   hasCharity,
   sellEstate,
   sellShares,
+  addCashFlow,
+  acceptFtDeal,
+  removePlayer,
   completeTurn,
+  acceptCharity,
+  rejectBigDeal,
   handleAuction,
   sellGoldCoins,
   selectBigDeal,
   acceptBigDeal,
-  acceptCharity,
-  rejectBigDeal,
+  rollDiceForMLM,
   addToFastTrack,
   isSharePresent,
   declineCharity,
@@ -53,20 +58,17 @@ const {
   selectSmallDeal,
   isAbleToDoCharity,
   provideLiabilities,
+  renderRequesterData,
   provideCommonEstates,
   rollDiceForSplitReverse,
-  getPlayersFinancialStatement,
-  rollDiceForMLM,
-  removePlayer,
-  renderRequesterData,
-  acceptFtDeal,
-  passDeal
+  getPlayersFinancialStatement
 } = require("./gameHandlers");
 
+app.fs = fs;
 app.games = {};
 app.createGameId = createGameId;
-app.fs = fs;
 loadSavedGames();
+
 app.use(logRequest);
 app.use(cookieParser());
 app.use(getCurrentGame);
@@ -76,31 +78,31 @@ app.use(bodyParser.json());
 app.get("/", renderHomePage);
 app.get("/acceptFtDeal", acceptFtDeal);
 app.get("/getgame", getGame);
+app.get("/savegame", saveGame);
+app.get("/passdeal", passDeal);
 app.get("/startgame", startGame);
 app.get("/hascharity", hasCharity);
+app.get("/cancelgame", cancelGame);
+app.get("/leavegame", removePlayer);
+app.get("/addcashflow", addCashFlow);
 app.get("/completeturn", completeTurn);
 app.get("/gamelobby", provideGameLobby);
 app.get("/selectBigDeal", selectBigDeal);
 app.get("/acceptCharity", acceptCharity);
 app.get("/acceptBigDeal", acceptBigDeal);
 app.get("/declineBigDeal", rejectBigDeal);
+app.get("/addtofasttrack", addToFastTrack);
+app.get("/requester", renderRequesterData);
 app.get("/issharepresent", isSharePresent);
+app.get("/rolldiceformlm", rollDiceForMLM);
 app.get("/declineCharity", declineCharity);
 app.get("/liabilities", provideLiabilities);
 app.get("/selectSmallDeal", selectSmallDeal);
-app.get("/addtofasttrack", addToFastTrack);
 app.get("/acceptSmallDeal", acceptSmallDeal);
 app.get("/declineSmallDeal", rejectSmallDeal);
 app.get("/commonestates", provideCommonEstates);
 app.get("/isabletodocharity", isAbleToDoCharity);
 app.get("/financialStatement", getPlayersFinancialStatement);
-app.get("/rolldiceformlm", rollDiceForMLM);
-app.get("/leavegame", removePlayer);
-app.get("/savegame", saveGame);
-app.get("/requester", renderRequesterData);
-app.get("/cancelgame", cancelGame);
-app.get("/addcashflow", addCashFlow);
-app.get("/passdeal", passDeal);
 
 app.post("/paydebt", payDebt);
 app.post("/hostgame", hostGame);
